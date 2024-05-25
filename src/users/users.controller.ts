8d4ec8ab84abf0c997bbbef16c8f058e7b16167c
Héctor Plinio Navarro Navarro <73659203+hectorplinio@users.noreply.github.com>
2024-05-25 12:26:07 +0200

    update user info

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Body, ConflictException, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  ConflictException,
+  Controller,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 import { UsersService } from './users.service';
 import { YupValidationPipe } from '../common/yup-validation.pipe';
@@ -8,13 +17,22 @@ import {
   ApiConflictResponse,
   ApiCreatedResponse,
   ApiOperation,
+  ApiParam,
+  ApiResponse,
   ApiTags,
+  ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
+import { UpdateUserDto, updateUserSchema } from './update-user.dto';
+import { AuthUserDto, authUserSchema } from '../auth/auth-users.dto';
+import { AuthService } from '../auth/auth.service';
 
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Post()
   @ApiOperation({ summary: 'Create new user' })
@@ -58,6 +76,74 @@ export class UsersController {
     } catch (error) {
       if (error instanceof ConflictException) {
         throw new ConflictException('Email already exists');
+      }
+      throw error;
+    }
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: 'Update user data' })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'User ID',
+    schema: { type: 'string', example: 'uuid' },
+  })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        email: { type: 'string', example: 'user@example.com' },
+        name: { type: 'string', example: 'Updated User Name' },
+        password: { type: 'string', example: 'newpassword123' },
+      },
+      required: ['email', 'name', 'password'],
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Updated',
+    schema: {
+      example: {
+        email: 'user@example.com',
+        name: 'Updated User Name',
+        password: 'newpassword123',
+        id: 'uuid',
+      },
+    },
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Unauthorized',
+    schema: {
+      example: {
+        statusCode: 401,
+        message: 'Invalid credentials',
+        error: 'Unauthorized',
+      },
+    },
+  })
+  update(
+    @Param('id') id: string,
+    @Body(new YupValidationPipe(updateUserSchema)) updateUserDto: UpdateUserDto,
+    @Body(new YupValidationPipe(authUserSchema)) authUserDto: AuthUserDto,
+  ) {
+    try {
+      this.authService.validateAndLogin({
+        id: id,
+        ...authUserDto,
+      });
+      const updatedUser = this.usersService.update(id, updateUserDto);
+
+      return { ...updatedUser };
+    } catch (error) {
+      if (error instanceof ConflictException) {
+        throw new ConflictException('Email already exists');
+      }
+      if (error instanceof NotFoundException) {
+        throw new NotFoundException('User not found');
+      }
+      if (error instanceof UnauthorizedException) {
+        throw new UnauthorizedException('Invalid credentials');
       }
       throw error;
     }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,11 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './create-users.dto';
 import { v4 as uuidv4 } from 'uuid';
+import { UpdateUserDto } from './update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -19,7 +24,27 @@ export class UsersService {
     return user;
   }
 
+  update(id: string, updateUserDto: UpdateUserDto) {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+      throw new NotFoundException('User not found');
+    }
+
+    const updatedUser = {
+      ...this.users[userIndex],
+      ...updateUserDto,
+      id: this.users[userIndex].id,
+    };
+
+    this.users[userIndex] = updatedUser;
+    return updatedUser;
+  }
+
   findOneByEmail(email: string) {
     return this.users.find((user) => user.email === email);
+  }
+
+  findOneById(id: string) {
+    return this.users.find((user) => user.id === id);
   }
 }
